c9a286f7aed772874ea14c26559a6867032c5366
man18008 <man18008@byui.edu>
2021-10-07 10:28:54 -0600

    documents

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Document } from '../document.model';
+import * as uuid from 'uuid';
 
 @Component({
   selector: 'cms-document-list',
@@ -7,6 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DocumentListComponent implements OnInit {
 
+  @Output() selectedDocumentEvent: EventEmitter<Document> = new EventEmitter<Document>();
+
+  documents: Document[] = [
+    new Document('0', 'CIT 260 - Object Oriented Programming', 'In this course you will learn Object Oriented Programming and the Java programming language by designing and creating a simple game.', 'https://content.byui.edu/file/22c0260d-e1b7-43a2-8903-8d8f948041ee/4/syllabus.html', []),
+    new Document('1', 'CIT 366 - Full Web Stack Development', 'This course will teach you how to design and build interactive web based applications using HTML, CSS, JavaScript, and a web development stack.', 'https://www.byui.edu/', []),
+    new Document('2', 'CIT 425 - Data Warehousing', 'This course defines the theory and practice of data analysis. The course will compare and contrast the operational and analytical database models. Students will learn how to define, implement and query a database warehouse by leveraging sample data warehouses built from Enterprise Resource Planning (ERP) and Customer Resource Management (CRM) solutions.', 'https://www.byui.edu/', []),
+    new Document('3', 'CIT 460 - Enterprise Development', ' This course covers the architecture for N-tier applications by focusing on the use of effective design patterns. Different technologies to implement the MVC control pattern will be explored.', 'https://www.byui.edu/', []),
+    new Document('4', 'CIT 495 - Senior Practicum', 'This is a capstone experience for the Computer Information Technology major. There are two options available: A research paper on a relevant Information Technology topic or participate in service learning. The purpose of this course is to build on the knowledge that students have learned in the Computer Information Technology major.', 'https://www.byui.edu/', []),
+  ];
   constructor() { }
 
   ngOnInit(): void {
