64d667f7f72692cabf6a7802f1adc59f55d7e97c
meganschauersnavex <megan.schauers@navexglobal.com>
2021-10-29 14:53:40 -0600

    added a Form to Add and Edit Documents, working

--- src/app/documents/document-edit/document-edit.component.ts
+++ src/app/documents/document-edit/document-edit.component.ts
@@ -1,15 +1,62 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Document } from '../document.model';
+import { DocumentService } from '../document.service';
 
 @Component({
   selector: 'cms-document-edit',
   templateUrl: './document-edit.component.html',
   styleUrls: ['./document-edit.component.css']
 })
+
 export class DocumentEditComponent implements OnInit {
+  originalDocument: Document | null = null;
+  document?: Document;
+  editMode = false;
+  id: string;
 
-  constructor() { }
+  // https://byui.instructure.com/courses/164460/pages/w08-assignment-instructions
 
+  constructor(
+    private documentService: DocumentService,
+    private router: Router,
+    private route: ActivatedRoute) {
+
+  }
+
+  // PseudoCode Here: https://byui.instructure.com/courses/164460/pages/w08-assignment-instructions
   ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      this.id = params.id;
+      if (!this.id) {
+        this.editMode = false;
+        return;
+      }
+      this.originalDocument = this.documentService.getDocument(this.id);
+      if (!this.originalDocument) {
+        return;
+      }
+      this.editMode = true;
+      this.document = JSON.parse(JSON.stringify(this.originalDocument));
+    });
+  }
+
+    // PseudoCode Here: https://byui.instructure.com/courses/164460/pages/w08-assignment-instructions
+  onSubmit(f: NgForm): void {
+    const value = f.value;
+    const newDocument = new Document(value.id, value.name, value.description, value.url, []);
+    if (this.editMode) {
+      this.documentService.updateDocument(this.originalDocument, newDocument);
+    } else {
+      this.documentService.addDocument(newDocument);
+    }
+    this.router.navigate(['documents']);
+  }
+
+    // PseudoCode Here: https://byui.instructure.com/courses/164460/pages/w08-assignment-instructions
+  onCancel(): void {
+    this.router.navigate(['documents']);
   }
 
 }
