bb23fafcf994bca762c27b63959f4268f5251a9e
meganschauersnavex <megan.schauers@navexglobal.com>
2021-11-04 20:56:39 -0600

    issue with messages

--- src/app/messages/message-list/message-list.component.ts
+++ src/app/messages/message-list/message-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Message } from '../message.model';
 import { MessageService } from './../message.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'cms-message-list',
@@ -10,12 +11,17 @@ import { MessageService } from './../message.service';
 
 export class MessageListComponent implements OnInit {
   messages: Message[] = [];
+  subscription: Subscription;
 
   constructor(private messageService: MessageService) { }
 
   ngOnInit(): void {
-    this.messages = this.messageService.getMessages().slice();
+    this.messages = this.messageService.getMessages();
     this.messageService.messageChangedEvent.subscribe((messages) => this.messages = messages.slice());
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
   }
 
   onAddMessage(message: Message): void {
--- src/app/messages/message.service.ts
+++ src/app/messages/message.service.ts
@@ -1,6 +1,8 @@
 import { EventEmitter, Injectable, Output } from '@angular/core';
 import { Message } from './message.model';
 import { MOCKMESSAGES } from './MOCKMESSAGES';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +10,33 @@ import { MOCKMESSAGES } from './MOCKMESSAGES';
 
 export class MessageService {
   @Output() messageChangedEvent: EventEmitter<Message[]> = new EventEmitter<Message[]>();
-
+  messageListChangedEvent = new Subject<Message[]>();
+  
   messages: Message[] = [];
+  maxMessageId: number = 0;
 
-  constructor() {
-    this.messages = MOCKMESSAGES;
+  constructor(private http: HttpClient) {
+    this.http.get<Message[]>('https://cmswdd430-default-rtdb.firebaseio.com/messages.json')
+      .subscribe((messagesList: Message[]) => {
+        this.messages = messagesList;
+        this.maxMessageId = this.getMaxId();
+        this.messages.sort((a, b) => parseInt(a.id) > parseInt(b.id) ? 1 : 0);
+        this.messageListChangedEvent.next(this.messages.slice());
+      },
+        (error: any) => {
+          console.log(error);
+        });
+  }
+
+  getMaxId(): number {
+    let maxId = 0;
+    for (const message of this.messages) {
+      let currentId = parseInt(message.id);
+      if (currentId > maxId) {
+        maxId = currentId;
+      }
+    }
+    return maxId;
   }
 
   getMessages(): Message[] {
@@ -28,9 +52,50 @@ export class MessageService {
     return null;
   }
 
-  addMessage(message: Message): void {
-    this.messages.push(message);
-    this.messageChangedEvent.emit(this.messages.slice());
+  addMessage(newMessage: Message) {
+    if (!newMessage) {
+      return;
+    }
+    this.maxMessageId++;
+    newMessage.id = this.maxMessageId.toString();
+    this.messages.push(newMessage);
+    this.storeMessages();
   }
 
+  updateMessage(originalMessage: Message, newMessage: Message) {
+    if (!originalMessage || !newMessage) {
+      return;
+    }
+    const position = this.messages.indexOf(originalMessage);
+    if (position < 0) {
+      return;
+    }
+    newMessage.id = originalMessage.id;
+    this.messages[position] = newMessage;
+    this.storeMessages();
+  }
+
+  deleteMessage(message: Message) {
+    if (!message) {
+      return;
+    }
+    const position = this.messages.indexOf(message);
+    if (position < 0) {
+      return;
+    }
+    this.messages.splice(position, 1);
+    this.storeMessages();
+  }
+
+
+  storeMessages() {
+    const messagesJson = JSON.stringify(this.messages);
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      })
+    }
+    this.http.put<Message[]>('https://cmswdd430-default-rtdb.firebaseio.com/messages.json', messagesJson, httpOptions).subscribe(() => this.messageListChangedEvent.next(this.messages.slice()));
+
+  }
 }
